7adb25394a085dacd78355b64fb8bae54cb6dddc
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-16 22:19:23 -0300

    ejemplo de templates

--- ttps_test/persons/views.py
+++ ttps_test/persons/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.shortcuts import render
+
 
 from .models import Person
 
@@ -11,5 +13,6 @@ def index(request):
     context = {
         'persons': Person.objects.all()
     }
-    template = loader.get_template('persons/index.tmpl')
-    return HttpResponse(template.render(context, request))
+    #template = loader.get_template('persons/index.tmpl')
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'persons/index.tmpl', context)
