fd602450f88763624a4eeadc44d0452249f4b0b2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-20 20:24:13 -0300

    ahora agrego namespaces a las urls

--- ttps_test/persons/urls.py
+++ ttps_test/persons/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 from . import views
 
+app_name='persons'
 urlpatterns = [
     path('', views.index),
     path('detail/<str:person_id>', views.detail, name='detail')
