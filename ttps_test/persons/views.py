a47a690449f9c653196c12e47e769ede6faf596d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-20 21:39:42 -0300

    agrego un ejemplo de como sacar json de una view

--- ttps_test/persons/views.py
+++ ttps_test/persons/views.py
@@ -2,13 +2,18 @@ import logging
 logging.getLogger().setLevel(logging.DEBUG)
 
 from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 from django.template import loader
 from django.urls import reverse
+from django.forms.models import model_to_dict
 
 from .models import Person, Identification
 
 # Create your views here.
+def test_json(request):
+    if 'json' in request.META['HTTP_ACCEPT']:
+        return True
+    return False
 
 def index(request):
     context = {
@@ -16,8 +21,11 @@ def index(request):
     }
     #template = loader.get_template('persons/index.tmpl')
     #return HttpResponse(template.render(context, request))
+    if test_json(request):
+        persons = [model_to_dict(p) for p in context['persons']]
+        return JsonResponse(persons, json_dumps_params={"ensure_ascii": False}, safe=False)
+    
     return render(request, 'persons/index.tmpl', context)
-
 
 def detail(request, person_id):
     logging.debug(f'buscando persona con id : {person_id}')
@@ -31,9 +39,10 @@ def detail(request, person_id):
     return render(request, 'persons/detail.tmpl', context)
 
 
-
-
 def add(request):
+    if request.method == 'GET':
+        return render(request, 'persons/add.tmpl')
+
     try:
         dni = request.POST['dni']
         name = request.POST['name']
@@ -41,4 +50,4 @@ def add(request):
         person = Person.objects.create_person(dni, name, lastname)
         return HttpResponseRedirect(reverse('persons:detail', args=(person.id,)))
     except KeyError:
-        return render(request, 'persons/add.tmpl')
+        raise Http404('Error')
