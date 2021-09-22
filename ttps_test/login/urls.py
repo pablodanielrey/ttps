8fb742e383d25106db1287c1ea42560c4919e000
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-09-21 21:29:09 -0300

    agrego un ejemplo basico de login

---
+++ ttps_test/login/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from . import views
+
+app_name='login'
+urlpatterns = [
+    path('', views.login_view),
+    path('login/', views.login_view)
+]
--- ttps_test/ttps_test/urls.py
+++ ttps_test/ttps_test/urls.py
@@ -21,6 +21,7 @@ from persons import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('login.urls')),
     path('persons/', include('persons.urls')),
     path('api/', include('api.urls'))
 
