035dd0e480d54e8d95c390b0247fa025b21d7da5
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-17 20:49:21 -0300

    agrego un ejemplo de formulario autogenerado a partir de un modelo

--- laboratorio/personas/views.py
+++ laboratorio/personas/views.py
@@ -1,15 +1,26 @@
+from django.forms.forms import Form
 from django.http.request import HttpRequest
 from django.shortcuts import render
+from django.forms import ModelForm
 
 from django.contrib import messages
+
+from . import models
 
 import logging
 # Create your views here.
 
+class FormularioPersona(ModelForm):
+    class Meta:
+        model = models.Persona
+        fields = ['usuario', 'dni', 'telefono', 'fecha_nacimiento', 'historia_clinica']
+
 def agregar_persona(request:HttpRequest):
     if request.method == 'POST':
-        logging.debug(request.POST['nombre'])
-        logging.debug(request.POST['apellido'])
+        datos = FormularioPersona(request.POST)
+        logging.debug(datos)
         messages.success(request, 'Paciente agregado correctamente')
 
-    return render(request, 'personas/agregar.html')
+    return render(request, 'personas/agregar.html', {
+        'form': FormularioPersona()
+    })
