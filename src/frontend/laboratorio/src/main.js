81182ac8a64f55b83c95b135b20f90bcee321463
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-18 13:59:45 -0300

    refactorice para instalar bootstrap en un archivo aparte

--- src/frontend/laboratorio/src/main.js
+++ src/frontend/laboratorio/src/main.js
@@ -1,25 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './bootstrap'
 import './veevalidate'
 import router from './router'
 
 const vueApp = createApp(App)
 vueApp.use(router)
 
-
-// seteo bootstrap
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-// Make BootstrapVue available throughout your project
-vueApp.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-vueApp.use(IconsPlugin)
-
-
-// monto la app
 vueApp.mount('#app')
 
-export default vueApp
