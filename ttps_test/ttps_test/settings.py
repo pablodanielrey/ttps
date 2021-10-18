faec35ff4bfcc8e71c17eb8ddd60103cfbca2fa9
MAGNANI, NICOLAS <nico_magnani@hotmail.com>
2021-10-17 21:21:18 -0300

    crear paciente

--- ttps_test/ttps_test/settings.py
+++ ttps_test/ttps_test/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'oauth2_provider',
-    'corsheaders',
+    'corsheaders',  
     'login.apps.LoginConfig',
     'persons.apps.PersonsConfig',
     'labs.apps.LabsConfig',
@@ -81,11 +81,17 @@ WSGI_APPLICATION = 'ttps_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'peliculas',
+        'USER':'root',
+        'PASSWORD':'',
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'charset': 'utf8mb4',
+            "autocommit": True,
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
