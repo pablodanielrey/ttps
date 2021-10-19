a32ef0aa5f263728a35f0e4cd4b7aeabb15f5a18
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-19 12:31:18 -0300

    inicio un proyecto desde cero usando vue2

--- src/frontend/laboratorio/src/App.vue
+++ src/frontend/laboratorio/src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <router-view/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
 <style>
 #app {
@@ -9,18 +23,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- src/frontend/laboratorio/src/main.js
+++ src/frontend/laboratorio/src/main.js
@@ -1,19 +1,8 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.config.productionTip = false
 
-// import './veevalidate'
-import router from './router'
-
-
-const vueApp = createApp(App)
-vueApp.use(BootstrapVue)
-vueApp.use(IconsPlugin)
-vueApp.use(router)
-
-vueApp.mount('#app')
-
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
