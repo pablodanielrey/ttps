2cb7212a398e97e11ef0e9cd42905ad40f90ecc0
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-19 16:22:05 -0300

    saco el * del cors

--- src/backend/laboratorio/laboratorio/settings.py
+++ src/backend/laboratorio/laboratorio/settings.py
@@ -158,6 +158,5 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    '*',
     'http://localhost:8080',
 ]
