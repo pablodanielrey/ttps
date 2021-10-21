ae3d0cda7796d683ef9dfa433491060e77943843
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-10-20 21:06:56 -0300

    elimino la parte de la relacion por lo menos para esta entrega paciente no va a ser usuario del sistema

--- src/backend/laboratorio/personas/models.py
+++ src/backend/laboratorio/personas/models.py
@@ -15,35 +15,6 @@ class ObraSocial(models.Model):
 class Persona(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # lo siguiente es para cuando se implemente el paciente como usuario.
-    # por ahora es un objeto de nuestro sistema. no un sujeto.
-    #     
-    # usuario = models.ForeignKey(User, on_delete=CASCADE, null=True)
-
-    # @property
-    # def nombre(self):
-    #     return self.usuario.first_name
-    
-    # @nombre.setter
-    # def __set_nombre(self, var):
-    #     self.usuario.first_name = var
-
-    # @property
-    # def apellido(self):
-    #     return self.usuario.last_name
-
-    # @apellido.setter
-    # def __set_apellido(self, var):
-    #     self.usuario.last_name = var
-
-    # @property
-    # def email(self):
-    #     return self.usuario.email
-
-    # @email.setter
-    # def __set_email(self, var):
-    #     self.usuario.email = var
-
     nombre = models.CharField(max_length=500)
     apellido = models.CharField(max_length=500)
     email = models.EmailField()
@@ -53,7 +24,6 @@ class Persona(models.Model):
     historia_clinica = models.CharField(max_length=9216, null=True)
 
     def __str__(self):
-        # return f"{self.usuario.__str__()} {self.nombre} {self.apellido} {self.dni}"
         return f"{self.nombre} {self.apellido} {self.dni}"
 
 
