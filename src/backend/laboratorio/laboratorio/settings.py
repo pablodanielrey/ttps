2fe13f9cc5e009956e059f55982a782255121bd3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-11-15 12:43:55 -0300

    agrego de nuevo postgres

--- src/backend/laboratorio/laboratorio/settings.py
+++ src/backend/laboratorio/laboratorio/settings.py
@@ -83,12 +83,18 @@ WSGI_APPLICATION = 'laboratorio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-   
-
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': BASE_DIR / 'db.sqlite3',
+    #  },
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laboratorio',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'laboratorio',
+        'PASSWORD': 'laboratorio'
+    }   
 }
 
 
