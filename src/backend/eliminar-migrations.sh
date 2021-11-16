ccff17f17cb85c026baf59743773ae0ae4c74ce2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-11-16 17:41:28 -0300

    agrego para eliminar las migraciones

---
+++ src/backend/eliminar-migrations.sh
@@ -0,0 +1,11 @@
+rm laboratorio/estudios/migrations/*
+rm laboratorio/login/migrations/*
+rm laboratorio/lotes/migrations/*
+rm laboratorio/personas/migrations/*
+rm laboratorio/turnos/migrations/*
+
+touch laboratorio/estudios/migrations/__init__.py
+touch laboratorio/login/migrations/__init__.py
+touch laboratorio/lotes/migrations/__init__.py
+touch laboratorio/personas/migrations/__init__.py
+touch laboratorio/turnos/migrations/__init__.py
