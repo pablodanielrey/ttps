08676a1dd71122958a43448ccb71eb0e5d832bc5
nicomagnani3 <nicolasmagnani3@gmail.com>
2021-11-16 17:41:17 -0300

    paciente

--- src/frontend/laboratorio/src/services/ObrasSocialesService.js
+++ src/frontend/laboratorio/src/services/ObrasSocialesService.js
@@ -1,14 +1,14 @@
 import Api from "@/services/Api"
 
 const API_USER = ''
-const API_URL = '/api/'
+const API_URL = '/personas_api/'
 
 export default {
     obtenerObrasSociales() {
-        return Api().get(API_URL + API_USER + "obrasSociales/")
+        return Api().get(API_URL + API_USER + "obras_sociales/")
     },
     crearObraSocial(obraSocial) {
-        return Api().post(API_URL + API_USER + "obrasSociales/", obraSocial);
+        return Api().post(API_URL + API_USER + "obras_sociales/", obraSocial);
     }
     
 }
