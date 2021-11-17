47007ab53644d52f9dc2526f173e47836eb6984b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-11-17 11:38:28 -0300

    agrego la api de configuradores

--- src/backend/laboratorio/personas/urls.py
+++ src/backend/laboratorio/personas/urls.py
@@ -7,10 +7,11 @@ from . import views_personas
 from . import views_pacientes
 from . import views_medicos
 from . import views_usuarios
-
+from . import views_configuradores
 
 router = routers.DefaultRouter()
 router.register('usuarios', views_usuarios.VistaUsuario)
+router.register('configuradores', views_configuradores.VistaConfigurador)
 router.register('personas', views_personas.VistaPersona)
 router.register('pacientes', views_pacientes.VistaPaciente)
 router.register('obras_sociales', views.VistaObraSocial)
