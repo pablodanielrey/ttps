4e9bb51bad8d207af25f25d3e9f148cd793b37e2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-06 20:33:28 -0300

    le agrego a leandro los datos del medico que los necesita en una pnatlala

--- src/backend/laboratorio/estudios/views.py
+++ src/backend/laboratorio/estudios/views.py
@@ -161,6 +161,7 @@ class SerializadorEsperandoProcesamientoDeLoteBiotecnologico(serializers.ModelSe
 
 
 class SerializadorEsperandoInterpretacionDeResultados(serializers.ModelSerializer):
+    medico_informante = SerializadorDePersona()
     class Meta:
         model = models.EsperandoInterpretacionDeResultados
         fields = ['id','fecha','fecha_informe','medico_informante','informe','resultado']
