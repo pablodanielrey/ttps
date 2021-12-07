8049b17708a0f6e9a1c17567f4869b25f6df57d1
lbilbao <leandrobilbao@gmail.com>
2021-12-06 21:12:38 -0300

    anteultimo estado al 70%

--- src/frontend/laboratorio/src/components/EstadosEstudio/EsperandoEntregaAMedicoDerivante.vue
+++ src/frontend/laboratorio/src/components/EstadosEstudio/EsperandoEntregaAMedicoDerivante.vue
@@ -1,21 +1,31 @@
 <template>
   <b-container>
-    <h3>Descargar resultado para enviarle al medico derivante</h3>
-    <b-button
-      title="Enviado a medico derivante"
-      variant="outline-success"
-      @click="bajarInforme()"
-    >
-      <b-icon icon="share" aria-hidden="true"></b-icon
-    ></b-button>
+    <div>
+      <h3>Descargar resultado para enviarle al medico derivante</h3>
+      <div>
+        <b-button
+          title="Enviado a medico derivante"
+          variant="outline-success"
+          @click="bajarInforme()"
+        >
+          <b-icon icon="share" aria-hidden="true"></b-icon
+        ></b-button>
+      </div>
+      <br/>
+      <div>
+        <b-button variant="success" @click="siguienteEstado()"
+          > Finalizar estudio
+        </b-button>
+      </div>
+    </div>
+    {{this.estudio}}
   </b-container>
 </template>
 
 <script>
-import { jsPDF } from "jspdf";
-/*  
+import jsPDF from "jspdf"; 
 import EstudiosService from "@/services/EstudiosService.js";
- */
+
 export default {
   components: {},
   props: {
@@ -28,8 +38,31 @@ export default {
       fecha_entrega: new Date(),
     };
   },
-  created() {},
+  
+
+  created() {
+    console.log(this.estudio)
+  },
+
   methods: {
+    async obtenerDetalleEstudio() {
+      try {
+        let response = await EstudiosService.obtenerEstudio(this.estudioId);
+        console.log(response);
+        this.estudio = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async obtenerDetalleEstados() {
+      try {
+        let response = await EstudiosService.obtenerEstudio(this.estudioId);
+        console.log(response);
+        this.estudio = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
     async bajarInforme() {
       /*     try {
           let datos={
@@ -64,27 +97,51 @@ export default {
           );
         } */
       console.log(this.estudio);
-      const doc = new jsPDF({});
-      doc.text(
-        "Estudio del paciente  " + this.estudio.paciente.apellido +' '+ this.estudio.paciente.nombre,
-        30,
-        20
-      );
-      doc.addFont("bromellonavidadregular", "bromellonavidadregular", "normal");
-      doc.setFont("bromellonavidadregular");
-      doc.setFontSize(13);
-            doc.text("Dni:  " + this.estudio.paciente.dni, 40, 30);
-
-      doc.text("Tipo de estudio:  " + this.estudio.tipo.nombre, 40, 50);
-      
-      doc.text("Diagnostico:  " + this.estudio.diagnostico.nombre, 40, 60);
-      doc.text("Resultado:  " + this.estudio.diagnostico.nombre, 40,70);
-
-      doc.save("InformeDelEstudio.pdf");
+      let pdfName = 'informeEstudio'; 
+      var doc = new jsPDF();
+      doc.text("Informe de estudio:", 10, 10);
+      doc.text("Paciente  " + this.estudio.paciente.apellido +' '+ this.estudio.paciente.nombre, 30, 20);
+      //doc.text("Dni:  " + this.estudio.paciente.dni, 40, 30);
+      doc.text("Diagnostico previo:  " + this.estudio.diagnostico.nombre, 40, 50);
+      doc.text("Medico Derivante:  " + this.estudio.medico_derivante.apellido +' '+ this.estudio.medico_derivante.nombre +' lic: '+ this.estudio.medico_derivante.licencia,40, 60);
+      doc.text("Tipo de estudio:  " + this.estudio.tipo.nombre, 40, 70);
+      //doc.text("Medico Informante:  " + this.estudio.medico_informante.nombre, 40, 70);
+      //fecha de elaboracion deinforme
+      //resultado
+      // descripcion resultado
+      //doc.text("Resultado:  " + this.estudio.diagnostico.nombre, 40,70);
+      doc.save(pdfName + ".pdf");
     },
   },
+  async siguienteEstado() {
+    try {
+      let datosConsentimiento = {
+        estudio_id: this.estudio.id,
+        fecha_enviado: new Date(),
+      };       
+      let response = await EstudiosService.actualizarUltimoEstado(
+        datosConsentimiento
+      );
+      console.log(response);
+      this.$router.push({
+        name: "listaEstudios",
+      });
+    } catch (error) {
+      console.log(error);
+      this.$root.$bvToast.toast(
+        "ocurrio un error mientras continuaba ",
+        {
+          title: "Atencion!",
+          toaster: "b-toaster-top-center",
+          solid: true,
+          variant: "danger",
+        }
+      );
+    }
+  },
   computed: {},
-  mounted() {},
+  mounted() {
+  },
 };
 </script>
 
