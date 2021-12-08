17e0fc4df2b65566c5c91006d57782f3793cb362
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-08 15:21:09 -0300

    corregi el cambio de clave del medico informante. tambien los datos. el usuario es solo lectura. tendria que charlarlo con los chicos

--- src/backend/laboratorio/personas/views_medicos.py
+++ src/backend/laboratorio/personas/views_medicos.py
@@ -21,7 +21,6 @@ class SerializadorDeMedicoDerivante(serializers.ModelSerializer):
         fields = ['id','nombre','apellido','email','matricula']
 
     def update(self, instance, validated_data):
-        logging.info(validated_data)
         matricula = validated_data.pop('matricula')
         instance.matricula.numero = matricula['numero']
         instance.matricula.save()
@@ -51,11 +50,25 @@ class VistaMedicoDerivante(viewsets.ModelViewSet):
 
 class SerializadorDeMedicoInformante(serializers.ModelSerializer):
     usuario = serializers.CharField(source='usuario.username', read_only=True)
-    clave = serializers.CharField(source='usuario.password', read_only=True)
-    matricula = serializers.CharField(source='matricula.numero', read_only=True)
+    clave = serializers.CharField(source='usuario.password', read_only=False)
+    matricula = serializers.CharField(source='matricula.numero', read_only=False)
     class Meta:
         model = models.MedicoInformante
         fields = ['id','nombre','apellido','email','matricula','usuario','clave']
+
+    def update(self, instance, validated_data):
+        logging.info(validated_data)
+        matricula = validated_data.pop('matricula')
+        instance.matricula.numero = matricula['numero']
+        instance.matricula.save()
+        
+        credenciales = validated_data.pop('usuario')
+        from django.contrib.auth.hashers import make_password
+        usuario_django = instance.usuario
+        usuario_django.password = make_password(credenciales['password'])
+        usuario_django.save()
+
+        return super().update(instance, validated_data)
 
 class VistaMedicoInformante(viewsets.ModelViewSet):
     queryset = models.MedicoInformante.all()
