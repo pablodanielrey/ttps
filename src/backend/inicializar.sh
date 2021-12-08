774ac556a83082f3d8a4134ca127574f29cad555
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-08 14:44:44 -0300

    no funcionno la actualizacion de la app mediante AppConfig asi que lo dejo por script separado. total ahora se realiza 1 sola vez

--- src/backend/inicializar.sh
+++ src/backend/inicializar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker-compose exec backend python3 manage.py makemigrations
+docker-compose exec backend python3 manage.py migrate
 docker-compose exec backend python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'nimda')"
 curl -u admin:nimda http://localhost:8000/admin_api/inicializar
 curl -u admin:nimda http://localhost:8000/admin_api/usuarios
--- src/backend/laboratorio/admin_laboratorio/apps.py
+++ src/backend/laboratorio/admin_laboratorio/apps.py
@@ -1,6 +1,23 @@
+import logging
+
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class AdminLaboratorioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_laboratorio'
+
+    """
+    @classmethod
+    def ready(cls):
+        user_model = get_user_model()
+        log = logging.getLogger(cls.label)
+
+        try:
+            if not user_model.objects.filter(username="admin").first():
+                log.info("Creando superusuario por defecto: adminy clave: nimda")
+                user_model.objects.create_superuser('admin', 'admin@admin.admin', 'nimda')
+        except Exception:
+            log.warn("No se pudo crear el usuario admin")
+    """
