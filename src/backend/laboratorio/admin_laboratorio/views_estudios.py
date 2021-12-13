cda4198356eeeb9e620e54937d7c7bc6d6e035e6
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-13 17:46:59 -0300

    implemente los ejemplos de los estados de los estudios para poder debuggear mejor las pantallas

--- src/backend/laboratorio/admin_laboratorio/views_estudios.py
+++ src/backend/laboratorio/admin_laboratorio/views_estudios.py
@@ -13,6 +13,7 @@ from zoneinfo import ZoneInfo
 
 from estudios import models as estudio_models
 from personas import models as persona_models
+from turnos import models as turnos_models
 
 def generar_fecha_now():
     return datetime.datetime.now(tz=ZoneInfo('America/Argentina/Buenos_Aires'))
@@ -31,76 +32,141 @@ def generar_estudio(medico, paciente):
     estudio.save()
     return estudio
 
+def generar_estudio_anulado_por_falta_de_pago(empleado, medico, paciente):
+    estudio = generar_estudio(medico, paciente)
+    estado = estudio_models.EsperandoComprobanteDePago(estudio=estudio, persona=empleado)
+    estado.save()
+    estado = estudio_models.AnuladorPorFaltaDePago(estudio=estudio, persona=empleado, fecha_procesado=generar_fecha_now())
+    estado.save()
+    
+
 def generar_estudios_estado1(empleado, medico, paciente):
     estudio = generar_estudio(medico, paciente)
+    estado = estudio_models.EsperandoComprobanteDePago(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
 
+def generar_estudios_estado2(empleado, medico, paciente):
     archivo = estudio_models.Archivo(contenido=pdf_minimo(), content_type='application/pdf', encoding='base64')
     archivo.save()
-
-    estado = estudio_models.EsperandoComprobanteDePago(comprobante=archivo, estudio=estudio, persona=empleado)
+    estudio = generar_estudios_estado1(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.comprobante = archivo
     estado.save()
+    estado = estudio_models.EnviarConsentimientoInformado(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado3(empleado, medico, paciente):
+    estudio = generar_estudios_estado2(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_enviado = generar_fecha_now()
+    estado.save()
+    estado = estudio_models.EsperandoConsentimientoInformado(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado4(empleado, medico, paciente):
+    archivo = estudio_models.Archivo(contenido=pdf_minimo(), content_type='application/pdf', encoding='base64')
+    archivo.save()
+    estudio = generar_estudios_estado3(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.consentimiento = archivo
+    estado.save()
+    estado = estudio_models.EsperandoSeleccionDeTurnoParaExtraccion(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado5(empleado, medico, paciente):
+    archivo = estudio_models.Archivo(contenido=pdf_minimo(), content_type='application/pdf', encoding='base64')
+    archivo.save()
+    turno = turnos_models.TurnoConfirmado(persona=paciente, inicio=generar_fecha_now(), fin=generar_fecha_now() + datetime.timedelta(hours=1))
+    turno.save()
+    estudio = generar_estudios_estado4(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.turno = turno
+    estado.save()
+    estado = estudio_models.EsperandoTomaDeMuestra(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudio_turno_expirado(empleado, medico, paciente):
+    estudio = generar_estudios_estado5(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.expirado = True
+    estado.save()
+    estado = estudio_models.EsperandoSeleccionDeTurnoParaExtraccion(estudio=estudio, persona=empleado)
+    estado.save()    
+
+def generar_estudios_estado6(empleado, medico, paciente):
+    estudio = generar_estudios_estado5(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_muestra = generar_fecha_now()
+    estado.milimetros = 3.4
+    estado.freezer = '10e'
+    estado.save()
+    estado = estudio_models.EsperandoRetiroDeExtaccion(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado7(empleado, medico, paciente):
+    estudio = generar_estudios_estado6(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_retiro = generar_fecha_now()
+    estado.extracionista = 'pepe se lo lleva'
+    estado.save()
+    estado = estudio_models.EsperandoLoteDeMuestraParaProcesamientoBiotecnologico(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado8(empleado, medico, paciente):
+    estudio = generar_estudios_estado7(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.numero_lote = 'loteeeeee'
+    estado.save()
+    estado = estudio_models.EsperandoProcesamientoDeLoteBiotecnologico(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado9(empleado, medico, paciente):
+    estudio = generar_estudios_estado8(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_resultado = generar_fecha_now()
+    estado.resutlado_url = 'http://www.google.com/'
+    estado.save()
+    estado = estudio_models.EsperandoInterpretacionDeResultados(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado10(empleado, medico, paciente):
+    medico_informante = persona_models.MedicoInformante.objects.first()
+    estudio = generar_estudios_estado9(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_informe = generar_fecha_now()
+    estado.medico_informante = medico_informante
+    estado.informe = pdf_minimo()
+    estado.resultado = 'un resultado'
+    estado.save()
+    estado = estudio_models.EsperandoEntregaAMedicoDerivante(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
+
+def generar_estudios_estado11(empleado, medico, paciente):
+    estudio = generar_estudios_estado10(empleado, medico, paciente)
+    estado = estudio.ultimo_estado
+    estado.fecha_entrega = generar_fecha_now()
+    estado.save()
+    estado = estudio_models.ResultadoDeEstudioEntregado(estudio=estudio, persona=empleado)
+    estado.save()
+    return estudio
 
 
-
-def generar_estudio_de_muestra():
-
-
-    empleado = persona_models.Persona.objects.all().first()
-
-    paciente = persona_models.Paciente.objects.first()
-    p = estudio_models.EsperandoComprobanteDePago()
-
-
-
-    # p1 = persona_models.Paciente(nombre='Paciente', apellido='Cero', dni='000001', email='paciente@cero.com', telefono='221-1112233', fecha_nacimiento='1980-12-01')
-    # p1.save()
-    # mm = persona_models.Paciente(nombre='Medi', apellido='Cote', dni='2212211', email='m@hotmail.com', telefono='221-1112233', fecha_nacimiento='1995-06-02')
-    # mm.save()
-
-    # ob_social = persona_models.ObraSocial.objects.all().first()
-    # obp = persona_models.ObraSocialPersona(persona=p1, obra_social=ob_social, numero_afiliado='afiliate12345')
-    # obp.save()
-
-    # tipoe = estudio_models.TiposDeEstudio.objects.all().first()
-    # diagnostico = estudio_models.Diagnostico.objects.all().first()
-    # estudio = estudio_models.Estudio(paciente=p1,  tipo=tipoe, medico_derivante=mm, diagnostico=diagnostico)
-    # estudio.save()
-
-
-    # estudio_models.EsperandoRetiroDeExtaccion(persona=empleado, estudio=estudio, extracionista='pepe se la lleva a la muestra', fecha_retiro=generar_fecha_now()).save()
-    # estudio_models.EsperandoLoteDeMuestraParaProcesamientoBiotecnologico(estudio=estudio, persona=empleado).save()
-    
-
-
-    # #estudio_models.EsperandoPresupuesto(persona=empleado, estudio=estudio, presupuesto=10.3).save()
-    # #estudio_models.EsperandoFactura(persona=empleado, estudio=estudio, numero='dsaasd324324', monto=10.5).save()
-    # #estudio_models.EsperandoFactura(persona=empleado, estudio=estudio, numero='dsaasd324325', monto=11.5, obra_social=ob_social).save()
-    # estudio_models.EsperandoComprobanteDePago(persona=empleado, estudio=estudio, comprobante='base64-del-comprobante').save()
-    # estudio_models.AnuladorPorFaltaDePago(persona=empleado, estudio=estudio).save()
-
-
-    # tipoe = estudio_models.TiposDeEstudio.objects.all().first()
-    # diagnostico = estudio_models.Diagnostico.objects.all().first()
-    # estudio = estudio_models.Estudio(paciente=p1,  tipo=tipoe, medico_derivante=mm, diagnostico=diagnostico)
-    # estudio.save()
-
-    # #estudio_models.EsperandoPresupuesto(persona=empleado, estudio=estudio, presupuesto=10.3).save()
-    # #estudio_models.EsperandoFactura(persona=empleado, estudio=estudio, numero='dsaasd324325', monto=11.5, obra_social=ob_social).save()
-    # estudio_models.EsperandoComprobanteDePago(persona=empleado, estudio=estudio, comprobante='base64-del-comprobante').save()
-    # estudio_models.EsperandoConsentimientoInformado(persona=empleado, estudio=estudio, consentimiento='base64-del-consntimiento').save()
-
-    # #estudio_models.EsperandoSeleccionDeTurnoParaExtraccion(persona=empleado, estudio=estudio, turno=generar_fecha_now()).save()
-    # estudio_models.EsperandoTomaDeMuestra(persona=empleado, estudio=estudio, expirado=True).save()
-    
-    # #estudio_models.EsperandoSeleccionDeTurnoParaExtraccion(persona=empleado, estudio=estudio, turno=generar_fecha_now()).save()
-    # estudio_models.EsperandoTomaDeMuestra(persona=empleado, estudio=estudio, fecha_muestra=generar_fecha_now(), mililitros=145, freezer=10, expirado=False).save()
-    # estudio_models.EsperandoRetiroDeExtaccion(persona=empleado, estudio=estudio, extracionista='pepe se la lleva a la muestra', fecha_retiro=generar_fecha_now()).save()
-    
-    # estudio_models.EsperandoLoteDeMuestraParaProcesamientoBiotecnologico(persona=empleado, estudio=estudio,numero_lote='2ef').save()
-    # estudio_models.EsperandoProcesamientoDeLoteBiotecnologico(persona=empleado, estudio=estudio, resultado_url='https://www.google.com/informe.pdf', fecha_resultado=generar_fecha_now()).save()
-    # estudio_models.EsperandoInterpretacionDeResultados(persona=empleado, estudio=estudio, fecha_informe=generar_fecha_now(), medico_informante=mm, informe='estas recontra bien. andate de vacaciones').save()
-    # estudio_models.EsperandoEntregaAMedicoDerivante(persona=empleado, estudio=estudio, fecha_entrega=generar_fecha_now()).save()
-
+def eliminar_estudios():
+    for estados in estudio_models.EstadoEstudio.objects.all():
+        estados.delete()
+        
+    for e in estudio_models.Estudio.objects.all():
+        e.delete()
 
 
 class Ejemplos(APIView):
@@ -114,10 +180,20 @@ class Ejemplos(APIView):
         paciente = persona_models.Paciente.objects.first()
 
         generar_estudios_estado1(empleado, medico_derivante, paciente)
-
+        generar_estudios_estado2(empleado, medico_derivante, paciente)
+        generar_estudios_estado3(empleado, medico_derivante, paciente)
+        generar_estudios_estado4(empleado, medico_derivante, paciente)
+        generar_estudios_estado5(empleado, medico_derivante, paciente)
+        generar_estudios_estado6(empleado, medico_derivante, paciente)
+        generar_estudios_estado7(empleado, medico_derivante, paciente)
+        generar_estudios_estado8(empleado, medico_derivante, paciente)
+        generar_estudios_estado9(empleado, medico_derivante, paciente)
+        generar_estudios_estado10(empleado, medico_derivante, paciente)
+        generar_estudios_estado11(empleado, medico_derivante, paciente)
 
         return Response({'status':'ejemplos de estudios generados'})
 
     def delete(self, request, format=None):
+        eliminar_estudios()
         return Response({'status':'ejemplos eliminados'})
 
