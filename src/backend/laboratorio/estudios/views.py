5e2ce122d6131ee2958cacca956caa95db7dccbf
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-13 21:16:34 -0300

    agrego una verificacion cuando se cambia el estado de esperandocomporbante depago

--- src/backend/laboratorio/estudios/views.py
+++ src/backend/laboratorio/estudios/views.py
@@ -255,21 +255,22 @@ class VistaEstadoEstudio(viewsets.ModelViewSet):
 
         """ aca manejo comportamientos especiales de los estados """
         if clase_ultimo_estado == models.EsperandoComprobanteDePago:
-
             """ solo pueden darse 2 casos. 1 - comprobante de pago, 2 - anulado por falta de pago """
-
-            if 'comprobante' in request.data:
-                archivo = models.Archivo.from_datauri(request.data['comprobante'])
-                archivo.save()
-                ultimo_estado.comprobante = archivo
-                ultimo_estado.save()
-
             if 'fecha_procesado' in request.data:
                 estado = models.AnuladorPorFaltaDePago(estudio=estudio, fecha_procesado=request.data['fecha_procesado'])
                 estado.save()
                 estudio.estados.add(estado)
                 serializador = SerializadorEstadoEstudioPolimorfico(estado, context={'request': request})
                 return Response(serializador.data)
+
+            elif 'comprobante' in request.data:
+                archivo = models.Archivo.from_datauri(request.data['comprobante'])
+                archivo.save()
+                ultimo_estado.comprobante = archivo
+                ultimo_estado.save()
+
+            else:
+                return HttpResponseBadRequest()
 
         elif clase_ultimo_estado == models.EsperandoConsentimientoInformado:
             archivo = models.Archivo.from_datauri(request.data['consentimiento'])
