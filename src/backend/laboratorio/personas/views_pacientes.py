83ef65ea9e47e3f31fd6405d84ee669412e5ad2f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-17 17:07:09 -0300

    agrego el codigo que faltaba para acomodar la vista. esta funcional. solo falta lo del tutor a menores de 18 años

--- src/backend/laboratorio/personas/views_pacientes.py
+++ src/backend/laboratorio/personas/views_pacientes.py
@@ -9,46 +9,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from . import models
-from . import views_personas
 from . import paciente_serializers
 
-
 class VistaPaciente(viewsets.ModelViewSet):
-    """
-        TODO: esto es necesario cambiarlo para algo parecido a :
-        https://www.django-rest-framework.org/api-guide/serializers/#dealing-with-nested-objects
-    """
     queryset = models.Paciente.all()
     serializer_class = paciente_serializers.SerializadorDePaciente
-    #permission_classes = [ DjangoModelPermissions ]
-    #permission_classes = [ IsAdminUser ]
-
-    def create(self, request, *args, **kwargs):
-        datos_persona = request.data
-
-        logging.debug(datos_persona)
-
-        obra_social = None
-        if 'obra_social' in datos_persona:
-            obra_social = request.data.pop("obra_social")
-            if obra_social:
-                afiliado = request.data.pop("numero_afiliado")
-
-        # paciente = self.model.crearPaciente(**request.data)
-        paciente = None
-
-        if obra_social:
-            obraSocial = models.ObraSocial.objects.get(id=obra_social)
-
-            pacienteObraSocial = models.ObraSocialPersona(
-                persona=paciente,
-                obra_social=obraSocial,
-                numero_afiliado=afiliado
-            )
-            pacienteObraSocial.save()
-
-        serializer = paciente_serializers.SerializadorDePaciente(paciente, context={'request': request})
-        return Response(serializer.data)
 
     @action(detail=False, methods=['GET'])
     def buscar(self, request):
@@ -58,8 +23,3 @@ class VistaPaciente(viewsets.ModelViewSet):
         personas = models.Paciente.buscar(q)
         serializer = paciente_serializers.SerializadorDePaciente(personas, many=True, context={'request': request})
         return Response(serializer.data)        
-
-class VistaPaciente2(viewsets.ModelViewSet):
-    queryset = models.Paciente.all()
-    serializer_class = paciente_serializers.SerializadorDePaciente
-
