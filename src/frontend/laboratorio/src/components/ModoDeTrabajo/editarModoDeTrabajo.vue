3df99d0410d70bcaac1d27ffc53ea6cdd9ed8ee5
lbilbao <leandrobilbao@gmail.com>
2021-12-19 20:43:22 -0300

    ver

--- src/frontend/laboratorio/src/components/ModoDeTrabajo/editarModoDeTrabajo.vue
+++ src/frontend/laboratorio/src/components/ModoDeTrabajo/editarModoDeTrabajo.vue
@@ -4,11 +4,7 @@
       <b-card header="Modo de Trabajo">
         <div>
           <div>
-            <select v-model="selected">
-                <option selected="selected">Paciente Obligado a completar los datos</option>
-                <option>Paciente y Empleado pueden completar</option> 
-            </select>
-<span>Seleccionado: {{ selected }}</span>
+
           </div>
         </div>
       </b-card>
@@ -18,71 +14,22 @@
 
 
 <script>
-import TemplateConsentimientoService from "@/services/TemplateConsentimiento.js";
+import ModoDeTrabajoService from "@/services/ModoDeTrabajo.js";
 
 export default {
   mounted() {},
 
   components: {},
   props: {
-    templateConsentimiento: {
-      type: Object,
-    },
   },
   created() {},
-  data() {
-    return {
-      file1: [],
-      archivo: null,
-    };
-  },
+  data() {},
 
   methods: {
-    obtenerPDF(event) {
-      const file = event.target.files[0];
-      this.createBase64(file);
-    },
-    createBase64(file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.archivo = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    },
-    async guardarTemplate() {
-      let result = await this.$refs.detailsTemplate.validate();
-      if (result) {
-        try {
-          let datosTemplate = this.archivo;
-          console.log(datosTemplate);
-          await TemplateConsentimientoService.editarTemplateConsentimiento(
-            datosTemplate
-          );
-
-          this.$root.$bvToast.toast("Se agrego el template", {
-            title: "Atencion!",
-            toaster: "b-toaster-top-center",
-            solid: true,
-            variant: "success",
-          });
-        } catch (err) {
-          console.log(err);
-          this.$root.$bvToast.toast(
-            "ocurrio un error mientras agregaba el archivo",
-            {
-              title: "Atencion!",
-              toaster: "b-toaster-top-center",
-              solid: true,
-              variant: "danger",
-            }
-          );
-        }
-      }
-    },
-    async verTemplate() {
+    async verModo() {
       try {
-        let response =
-          await TemplateConsentimientoService.obtenerTemplateConsentimiento();
+        let response = await ModoDeTrabajoService.obtenerModo();
+        this.modo = response.modo_operacion
         console.log(response);
       } catch (error) {
         console.log(error);
--- src/frontend/laboratorio/src/services/ModoDeTrabajo.js
+++ src/frontend/laboratorio/src/services/ModoDeTrabajo.js
@@ -1,11 +1,12 @@
 import Api from "@/services/Api"
 
 const API_USER = ''
-const API_URL = '/estudios_api/'
+const API_URL = '/admin_api/'
 
 export default {
     obtenerModo() {
-        return Api().get(API_URL + API_USER + "modo/")
+        return Api().get(API_URL + API_USER + "configuracion/")
+        
     },
     habilitarModo1() {
         return Api().post(API_URL + API_USER + "modo/");
