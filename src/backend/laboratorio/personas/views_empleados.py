e1fb55490f52112dcf6916942686efae4126dfda
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-19 18:00:42 -0300

    agrego el nuevo serializador de empleado

--- src/backend/laboratorio/personas/views_empleados.py
+++ src/backend/laboratorio/personas/views_empleados.py
@@ -7,27 +7,28 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from . import models
+from . import empleados_serializers
 
 
 
-class SerializadorDeEmpleado(serializers.ModelSerializer):
-    usuario = serializers.CharField(source='usuario.username', read_only=True)
-    clave = serializers.CharField(source='usuario.password', read_only=True)
-    class Meta:
-        model = models.Empleado
-        fields = ['id','nombre','apellido','usuario','clave']
+# class SerializadorDeEmpleado(serializers.ModelSerializer):
+#     usuario = serializers.CharField(source='usuario.username', read_only=True)
+#     clave = serializers.CharField(source='usuario.password', read_only=True)
+#     class Meta:
+#         model = models.Empleado
+#         fields = ['id','nombre','apellido','usuario','clave']
 
 class VistaEmpleado(viewsets.ModelViewSet):
     queryset = models.Empleado.all()
-    serializer_class = SerializadorDeEmpleado
+    serializer_class = empleados_serializers.SerializadorDeEmpleado
 
-    def create(self, request):
-        serializador = self.serializer_class(data=request.data)
-        serializador.is_valid()
-        serializador.save()
-        # configurador = self.model.crearEmpleado(**request.data)
-        # serializador = self.serializer_class(instance=configurador, context={'request':request})
-        return Response(serializador.data)
+    # def create(self, request):
+    #     serializador = self.serializer_class(data=request.data)
+    #     serializador.is_valid()
+    #     serializador.save()
+    #     # configurador = self.model.crearEmpleado(**request.data)
+    #     # serializador = self.serializer_class(instance=configurador, context={'request':request})
+    #     return Response(serializador.data)
 
     @action(detail=False, methods=['GET'])
     def buscar(self, request):
@@ -35,5 +36,5 @@ class VistaEmpleado(viewsets.ModelViewSet):
         logging.debug(f'buscando empleado : {q}')
 
         personas = models.Empleado.buscar(q)
-        serializer = SerializadorDeEmpleado(personas, many=True, context={'request': request})
+        serializer = empleados_serializers.SerializadorDeEmpleado(personas, many=True, context={'request': request})
         return Response(serializer.data)
