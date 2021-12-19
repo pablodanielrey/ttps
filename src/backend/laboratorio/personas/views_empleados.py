817b2efef40142e8d9042f54eaa5a35d0b662edb
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-19 18:01:09 -0300

    elmine el metodo que ya no era necesario

--- src/backend/laboratorio/personas/views_empleados.py
+++ src/backend/laboratorio/personas/views_empleados.py
@@ -10,25 +10,9 @@ from . import models
 from . import empleados_serializers
 
 
-
-# class SerializadorDeEmpleado(serializers.ModelSerializer):
-#     usuario = serializers.CharField(source='usuario.username', read_only=True)
-#     clave = serializers.CharField(source='usuario.password', read_only=True)
-#     class Meta:
-#         model = models.Empleado
-#         fields = ['id','nombre','apellido','usuario','clave']
-
 class VistaEmpleado(viewsets.ModelViewSet):
     queryset = models.Empleado.all()
     serializer_class = empleados_serializers.SerializadorDeEmpleado
-
-    # def create(self, request):
-    #     serializador = self.serializer_class(data=request.data)
-    #     serializador.is_valid()
-    #     serializador.save()
-    #     # configurador = self.model.crearEmpleado(**request.data)
-    #     # serializador = self.serializer_class(instance=configurador, context={'request':request})
-    #     return Response(serializador.data)
 
     @action(detail=False, methods=['GET'])
     def buscar(self, request):
