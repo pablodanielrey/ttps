a228ec1b9b3c2e7213edd12c40ebce2cfbacf75b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-12-21 22:15:01 -0300

    agrego la url del sitio por las dudas. pero eso deberia etar desactivado

--- src/backend/laboratorio/laboratorio/settings.py
+++ src/backend/laboratorio/laboratorio/settings.py
@@ -189,6 +189,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
+    'http://ttps-grupo07.dyndns.org',
     'http://localhost:8080'
     #'http://163.10.17.143:8080'
 ]
